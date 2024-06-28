f7652f2b18531cd20768b13eabe229645ed1307a
haemmerl <haemmerl@cis.lmu.de>
2024-06-28 13:29:13 +0200

    add splits to data collection script

--- ner/collect_data.py
+++ ner/collect_data.py
@@ -5,9 +5,17 @@ from datasets import load_dataset, concatenate_datasets, interleave_datasets
 LABELS_MASAKHANER = ["O", "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "B-DATE", "I-DATE"]
 
 
-def load_transform_uzb_gh():
+def load_transform_uzb_gh(split: str = "train"):
     # first clone https://github.com/azhar520/NER.git into ner folder
-    uz_gh = load_dataset("json", data_files="NER/data/train_enhanced.json", split="train")
+    if split == "train":
+        file = "NER/data/train_enhanced.json"
+    elif split == "dev":
+        file = "NER/data/dev.json"
+    elif split == "test":
+        file = "NER/data/test.json"
+    else:
+        raise ValueError("split must be 'train', 'dev', or 'test'")
+    uz_gh = load_dataset("json", data_files=file, split="train")
 
     def transform(entry):
         tokens = entry["sentence"]
@@ -29,9 +37,10 @@ def load_transform_uzb_gh():
     return uz_transformed
 
 
-def load_transform_masakhaner2(lang: str):  # particularly for yor and ibo
-    dataset = load_dataset("masakhane/masakhaner2", lang, split="train")
-
+def load_transform_masakhaner2(lang: str, split: str):  # particularly for yor and ibo
+    if split == "dev":
+        split = "validation"
+    dataset = load_dataset("masakhane/masakhaner2", lang, split=split)
     dataset_transformed = dataset.map(
         lambda x: {'ner_labels': [LABELS_MASAKHANER[i] for i in x['ner_tags']], "lang": lang},
         remove_columns=["ner_tags"]
@@ -41,9 +50,16 @@ def load_transform_masakhaner2(lang: str):  # particularly for yor and ibo
     return dataset_transformed
 
 
-def load_transform_polyglot(lang: str):  # particularly for tr and id
-    dataset = load_dataset("rmyeid/polyglot_ner", lang, split="train", trust_remote_code=True)
-    dataset = dataset.take(10000)  # may be larger but for now, eh
+def load_transform_polyglot(lang: str, split: str):  # particularly for tr and id
+    if split == "train":
+        split = "train[:10000]"
+    elif split == "dev":
+        split = "train[10000:11000]"
+    elif split == "test":
+        split = "train[11000:12000]"
+    else:
+        raise ValueError("split must be 'train', 'dev', or 'test'")
+    dataset = load_dataset("rmyeid/polyglot_ner", lang, split=split, trust_remote_code=True)
 
     def transform(entry):
         tokens = entry["words"]
@@ -78,29 +94,33 @@ def load_transform_swissner():
     return dataset_transformed
 
 
-def collect_data(datasets: Union[str, List[str]]):
+def collect_data(datasets: Union[str, List[str]], split: str = "train"):
     if datasets == "all":
-        # datasets = ["de_sw", "tr_polyglot", "id_polyglot", "yor_masakhaner2", "ibo_masakhaner2", "uzb_gh"]
-        datasets = ["de_DE", "tr_polyglot", "id_polyglot", "yor_masakhaner2", "ibo_masakhaner2", "uzb_gh"]
+        if split == "train":
+            datasets = ["de_DE", "tr_polyglot", "id_polyglot", "yor_masakhaner2", "ibo_masakhaner2", "uzb_gh"]
+        elif split == "dev":
+            datasets = ["de_sw", "tr_polyglot", "id_polyglot", "yor_masakhaner2", "ibo_masakhaner2", "uzb_gh"]
+        elif split == "test":
+            datasets = ["de_sw", "tr_polyglot", "id_polyglot", "yor_masakhaner2", "ibo_masakhaner2", "uzb_gh"]
     elif isinstance(datasets, str):
         datasets = [datasets]
 
     fetched = []
     for dataset in datasets:
         if dataset == "de_DE":
-            fetched.append((load_transform_polyglot("de")))
+            fetched.append((load_transform_polyglot("de", split=split)))
         if dataset == "de_sw":
             fetched.append(load_transform_swissner())
         if dataset == "tr_polyglot":
-            fetched.append(load_transform_polyglot("tr"))
+            fetched.append(load_transform_polyglot("tr", split=split))
         if dataset == "id_polyglot":
-            fetched.append(load_transform_polyglot("id"))
+            fetched.append(load_transform_polyglot("id", split=split))
         if dataset == "yor_masakhaner2":
-            fetched.append(load_transform_masakhaner2("yor"))
+            fetched.append(load_transform_masakhaner2("yor", split=split))
         if dataset == "ibo_masakhaner2":
-            fetched.append(load_transform_masakhaner2("ibo"))
+            fetched.append(load_transform_masakhaner2("ibo", split=split))
         if dataset == "uzb_gh":
-            fetched.append(load_transform_uzb_gh())
+            fetched.append(load_transform_uzb_gh(split=split))
 
     # all_datasets = concatenate_datasets(fetched).remove_columns("id").shuffle()
     # can be updated with probabilities to sub- or up-sample a bit
